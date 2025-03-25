b2e503ee023cd405e6f74bc6d5dbb205b8a031d6
Aftab Uddin <aftabbyofficial@gmail.com>
2025-03-25 11:25:37 +0600

    Added eda-data for model building

---
+++ scripts/model_building.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+def main():
+    pass
+
+
+def give_name_later(path="./../data/"):
+    pass
+
+
+if __name__ == "__main__":
+    main()
